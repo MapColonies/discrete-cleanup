1f1fe2f949de7b0462611a419f7c593d2b0a0486
Asaf Masa <ASAFMAS@rafael.co.il>
2022-01-04 08:05:52 +0200

    fix: fix config and injection

--- src/storageProviders/providersManager.ts
+++ src/storageProviders/providersManager.ts
@@ -8,14 +8,14 @@ import { S3TileStorageProvider } from './s3/s3TileStorageProvider';
 import { FsSourcesStorageProvider } from './fs/fsSourcesStorageProvider';
 
 export function getProviders(config: IConfig, logger: Logger): InjectionObject<unknown>[] {
-  const providers: InjectionObject<unknown>[] = [{ token: SERVICES.SOURCES_PROVIDER, provider: FsSourcesStorageProvider }];
+  const providers: InjectionObject<unknown>[] = [{ token: SERVICES.SOURCES_PROVIDER, provider: { useValue: new FsSourcesStorageProvider(logger, config)} }];
   const tilesProviderType = config.get<string>('tiles_storage_provider');
   switch (tilesProviderType.toUpperCase()) {
     case StorageProviderType.FS:
-      providers.push({ token: SERVICES.TILE_PROVIDER, provider: FsTileStorageProvider });
+      providers.push({ token: SERVICES.TILE_PROVIDER, provider: { useValue: new FsTileStorageProvider(logger, config)}});
       break;
     case StorageProviderType.S3:
-      providers.push({ token: SERVICES.TILE_PROVIDER, provider: S3TileStorageProvider });
+      providers.push({ token: SERVICES.TILE_PROVIDER, provider: { useValue: new S3TileStorageProvider(config, logger)}});
       break;
     default:
       logger.error(`invalid tile provider configuration: ${tilesProviderType}`);
