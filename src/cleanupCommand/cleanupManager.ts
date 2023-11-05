b90f1c131c4903208c0bda5564ec3c11ac309851
nitzanmorr <139204942+nitzanmorr@users.noreply.github.com>
2023-11-05 13:33:12 +0200

    chore: accept array values (MAPCO-3011) (#28)
    * chore:accept blacklist sources location as array
    
    * chore: added toJson function in configmap
    
    * fix: removed unnecessary log

--- src/cleanupCommand/cleanupManager.ts
+++ src/cleanupCommand/cleanupManager.ts
@@ -24,7 +24,7 @@ export class CleanupManager {
     this.discreteBatchSize = config.get<number>('batch_size.discreteLayers');
     this.newIngestionJobType = config.get('new_ingestion_job_type');
     this.updateIngestionJobType = config.get('update_ingestion_job_type');
-    this.sourceBlackList = config.get<string>('fs.blacklist_sources_location').split(',');
+    this.sourceBlackList = config.get<string[]>('fs.blacklist_sources_location');
     this.failedCleanupDelayDays = this.config.get<number>('failed_cleanup_delay_days.ingestion');
   }
 
